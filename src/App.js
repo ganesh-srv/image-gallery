9a0af84b6a8408965b14acc864badd51015e989c
ganesh-srv <ganeshsrv.sarakadam@gmail.com>
2024-02-27 18:01:04 -0500

    added my web component og-loader for testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Container from './Container/Container';
-
+import 'og-loader'
 
 function App() {
   return (
     <div className="App">
     <Container/>
+    {/* <og-loader color="black" speed="0.5s" show="true"></og-loader> */}
     </div>
   );
 }
